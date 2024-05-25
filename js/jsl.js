7d7a07c3294c6a4a38ae89760e45eafe953a5c97
tohobi.gawe <tohobi.gawe@gmail.com>
2024-05-25 17:39:18 +0000

    Inhalt der Vorlesung vom 30.04.2024 eingearbeitet

--- js/jsl.js
+++ js/jsl.js
@@ -11,6 +11,7 @@ class ViewController {
         this.prepareViewSwitching();
         this.prepareFading();
         this.prepareListItemSelection();
+        this.prepareAddingNewListItems();
     }
 
     prepareViewSwitching() {
@@ -38,12 +39,55 @@ class ViewController {
 
     prepareListItemSelection() {
         const listItems = this.root.getElementsByTagName("li");
-        for (let i = 0; i < listItems.length; i++) {
-            let currentLi = listItems[i];
-            currentLi.onclick = () => {
-                alert("selected: " + currentLi.querySelector("h2").textContent);
-            }
+        const onclickListener = (evt) => {
+            // console.log("evt: ", evt);
+            alert("selected: " + evt.target.closest("li").querySelector("h2").textContent);
+        };
+        const listRoot = this.root.querySelector("ul");
+        listRoot.onclick = onclickListener;
+  
+        // for (let i = 0; i < listItems.length; i++) {
+        //     let currentLi = listItems[i];
+        //     currentLi.onclick = (evt) => {
+        //         // console.log("evt: ", evt);
+        //         alert("selected: " + evt.target.querySelector("h2").textContent);
+        //     }
+        // }
+    }
+
+    prepareAddingNewListItems() {
+        const addingTrigger = this.root.querySelector(".myapp-img-add");
+        this.listRoot = this.root.querySelector("ul");
+        addingTrigger.onclick = (evt) => {
+            evt.stopPropagation();
+            // alert("add");
+            const srcoptions = ["1.jpg", "2.jpg", "3.jpg"];
+            const titleoptions = ["direm", "lopsum", "olor", "adispiscing", "elit", "consectetur"];
+
+            const selectedSrc = srcoptions[Date.now() % srcoptions.length];
+            const selectedTitle = titleoptions[Date.now() % titleoptions.length];
+
+            this.addNewListItem("./data/img/" + selectedSrc, selectedTitle);
         }
+    }
+
+    addNewListItem(src, title) {
+        // this.listRoot.innerHTML += "<li><img class=\"myapp-align-left\" src=\"" + src + "\" alt=\"\"><h2 class=\"myapp-align-left\">" + title + "</h2><button class=\"myapp-imgbutton myapp-img-edit myapp-align-right\">edit</button></li>";
+
+        const li = document.createElement("li");
+        const img = document.createElement("img");
+        li.appendChild(img);
+        const h2 = document.createElement("h2");
+        li.appendChild(h2);
+        const button = document.createElement("button");
+        li.appendChild(button);
+        img.src = src;
+        img.classList.add("myapp-align-left");
+        h2.textContent = title;
+        h2.classList.add("myapp-align-left");
+        button.setAttribute("class", "myapp-imgbutton myapp-img-edit myapp-align-right");
+
+        this.listRoot.appendChild(li);
     }
 
 }
